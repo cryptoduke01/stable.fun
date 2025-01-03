5655f7855d7609f73c5fcb0cbd7579a8ab09fb4c
Akachukwu <egbolucheakachukwu@gmail.com>
2025-01-03 21:24:51 +0100

    Added Analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import TokenFactory from "./components/TokenFactory";
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <div className="bg-black">
+      <Analytics />
       <Hero />
       <About />
       <TokenFactory />
